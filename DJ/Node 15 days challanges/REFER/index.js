770fd04824d480f42f501a6a3e5f696f4e2573a4
deepakjangid <winterjangid@gmail.com>
2024-04-10 00:37:54 +0530

    node 15 day challange

---
+++ DJ/Node 15 days challanges/REFER/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8000;
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
+require('dotenv').config();
+require('./db')
+
+app.use(cors());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use('/users', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the API'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
